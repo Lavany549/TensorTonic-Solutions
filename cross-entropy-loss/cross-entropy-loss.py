ac3627038897a0b5fec653ae479b257b70e2b2a1
Lavanya Sandula <47265625+Lavany549@users.noreply.github.com>
2026-05-10 20:34:59 +0530

    Update solution for cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -5,11 +5,8 @@ def cross_entropy_loss(y_true, y_pred):
     Compute average cross-entropy loss for multi-class classification.
     """
     # Write code here
+   
     result = np.array(y_pred)
-    eps = 1e-15
-    # result = np.where(result==1, 1-eps, result)
-    # result = np.where(result==0, eps, result)
-    result = np.array(result)
     y_true = np.array(y_true)
     N = y_true.size
     sum = 0 
